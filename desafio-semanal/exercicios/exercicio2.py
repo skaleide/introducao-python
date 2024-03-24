5783701493eaaa9f02d220cb473e2325c314e2ee
Skaleide <98890305+skaleide@users.noreply.github.com>
2024-03-24 15:46:04 -0300

    Update exercicio2.py

--- desafio-semanal/exercicios/exercicio2.py
+++ desafio-semanal/exercicios/exercicio2.py
@@ -5,3 +5,22 @@
 # - Mantenha uma variável para armazenar o preço total.
 # - Adicione o preço de cada fruta ao preço total.
 # - Após inserir todos os preços, imprima o preço total.
+
+def preco_total(preco_frutas) :
+     total = sum(preco_frutas)
+     return total
+
+def main():
+  preco_frutas = {}
+
+  while True:
+   fruta = input("Digite nome da fruta ou digiti 'fim'para encerrar")
+
+   if fruta == 'fim':
+     break
+   preco = float(input(" Digite o valor da fruta '{fruta}'"))
+   preco_frutas[fruta] = preco
+
+   total = preco_total(preco_frutas)
+   print("Preco total", total)
+  
