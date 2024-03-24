150cb2369ea50a2fb516388fc9a96f2ccfcac5c9
Skaleide <98890305+skaleide@users.noreply.github.com>
2024-03-24 15:05:26 -0300

    Update exercicio1.py

--- desafio-semanal/exercicios/exercicio1.py
+++ desafio-semanal/exercicios/exercicio1.py
@@ -3,5 +3,14 @@
 # Dicas que podem ser seguidas ou não: 
 # Remover duplicatas de uma lista:
 # - Você pode utilizar conjuntos (sets) para remover duplicatas, já que conjuntos não permitem elementos duplicados.
-# - Caso queira, converta a lista para um conjunto. Em seguida, converta o conjunto de volta para uma lista.
+# - Caso queira, converta a listapara um conjunto. Em seguida, converta o conjunto de volta para uma lista.
 # - Imprima a lista resultante.
+
+
+def remove_duplicatas(lista):
+    return list(set(lista))
+
+lista = [1, 2, 3, 4, 2, 3, 5]
+lista_sem_duplicatas = remove_duplicatas(lista)
+print("Lista original:", lista)
+print("Lista sem duplicatas:", lista_sem_duplicatas)
