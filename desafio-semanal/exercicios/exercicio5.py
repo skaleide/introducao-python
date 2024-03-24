58452545c1aafbf61b6c63b7646d3aa8494f5d33
Skaleide <98890305+skaleide@users.noreply.github.com>
2024-03-24 16:54:09 -0300

    Update exercicio5.py

--- desafio-semanal/exercicios/exercicio5.py
+++ desafio-semanal/exercicios/exercicio5.py
@@ -4,3 +4,13 @@
 # - Solicite ao usuário o preço original do produto e o desconto percentual ou armazene esses valores sem o input.
 # - Calcule o preço final aplicando o desconto percentual ao preço original.
 # - Imprima o preço final.
+
+
+preco_original = float(input("Digite o preço original do produto: "))
+desconto_percentual = float(input("Digite o desconto percentual (%): "))
+
+ 
+desconto_percentual = desconto_percentual/100
+preco_final = preco_original * (1 - desconto_percentual / 100)
+
+print("O preço final do produto após aplicar um desconto de", desconto_percentual, "% é:", preco_final)
